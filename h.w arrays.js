b27b29bbbd01169b446a221efc8973bbdddf393d
MahaKarban <maha.ahmad.karban@gmail.com>
2019-04-25 18:04:42 +0300

    solve h.w arrayss

---
+++ h.w arrays.js
@@ -0,0 +1,29 @@
+
+1.
+num = [1, 2, 3, 4, 5]
+var num = eval(arr.join('+'))
+
+2.
+var sum = 0;
+for(var x=2; x<=100; x+2)
+{
+  sum+=x;
+}
+return sum; 
+
+3.
+var num = [8, 2, 2, 4];
+for ( var i = 0; i < num.length; i ++){
+    var avg = (num[i] / num.length) * num.length
+}
+console.log(avg)
+
+4.
+function reverseString(str){ 
+    return str.split("").reverse().join("");   
+ }
+
+5.
+ var str = "test1-test2-test3-test4";
+ str.replace("-", ' ');
+ console.log(str);
