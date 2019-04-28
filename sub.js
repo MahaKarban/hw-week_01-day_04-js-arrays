479338af0053082e4d91a83a384aaf606bf0d259
MahaKarban <maha.ahmad.karban@gmail.com>
2019-04-28 17:56:20 +0300

    solution subway

---
+++ sub.js
@@ -0,0 +1,102 @@
+
+const stopsBetweenStations = function (StartLine, StartStation, EndLine, EndStation) {
+if (StartLine === EndLine && StartStation === EndStation)
+return "0 stop"
+else if (StartLine === EndLine && StartStation != EndStation)
+return "7 stop"
+if (StartLine != EndLine && StartStation != EndStation)
+return "6 stop"
+}
+
+
+
+
+/////// Try an other sulotion
+
+
+var RedStation = [station1,station2,station3,station4,station5,station6,station7,station8])
+ GreenStation = [station1,station2,station3,station4,station5,station6,station7])
+if (RedStation.station1[num]=RedStation.station1[num] )
+return 0
+else if (ReadStation.station8[num]<=RedStation.station1[num] )
+return (RedStation.station8[num] - RedStation.station1[num])
+else if (RedStation.station1[num] < GreenStation.station7[num] )
+return (RedStation.station1[num] - GreenStation.station7[num])
+
+    RedStation: [
+        {
+        station1: [{
+            name:'South Station',
+            num:1}]
+        
+        station2: [{
+            name:'Park Street',
+            num:2}]
+        
+        station3: [{
+            name:'Kendall',
+            num:3}]
+        
+        station4: [{
+            name:'Central',
+            num:4}]
+        
+        station5: [{
+            name:'Harvard',
+            num:5}]
+        
+        station6: [{
+            name:'Porter',
+            num:6}]
+        
+        station7: [{
+            name:'Davis',
+            num:7}]
+        
+        station8: [{
+            name:'Alewife',
+            num:8}]
+        
+    ]
+    
+
+
+    
+    GreenStation: [
+            {
+            station1: [{
+                name:'Government Center',
+                num:1}]
+            
+            station2: [{
+                name:'Park Street',
+                num:2}]
+            
+            station3: [{
+                name:'KBoylston',
+                num:3}]
+            
+            station4: [{
+                name:'Arlington',
+                num:4}]
+            
+            station5: [{
+                name:'Copley',
+                num:5}]
+            
+            station6: [{
+                name:'PHynes',
+                num:6}]
+            
+            station7: [{
+                name:'Kenmore',
+                num:7}]
+            
+    ]
+
+
+
+
+        
+                
+
